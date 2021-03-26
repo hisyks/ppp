60cca462258e5ef2f2be9e7cd5577dd5b1f7f629
hisyks <78439678+hisyks@users.noreply.github.com>
2021-03-26 19:59:09 +0200

    Create leanes.py

---
+++ leanes.py
@@ -0,0 +1,11 @@
+num1 = input("whats your firts number")
+num2 = input("whats your second number")
+op inout("whats your operastion")
+if op == "*":
+  print(num1 * num2)
+if op =="+":
+  print(num1 + num2)
+if op =="-":
+  print(num1 - num2)
+if op == "/":
+   print(num1 / num2)
