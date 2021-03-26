e7838eb61e24632d43b95aeb9cbd86efa7817f17
hisyks <78439678+hisyks@users.noreply.github.com>
2021-03-26 20:18:08 +0200

    Update leanes.py

--- leanes.py
+++ leanes.py
@@ -1,6 +1,6 @@
 num1 = input("whats your firts number")
 num2 = input("whats your second number")
-op input("whats your operastion")
+op = input("whats your operastion")
 if op == "*":
   print(num1 * num2)
 if op =="+":
